987a00ad84fbd5ea9428a78b080e5bd15891fb11
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2024-05-07 22:52:02 -0400

    initializing

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = ["Romeo and Juliet", "Hamlet", "Pride and Prejudice", 
+"Moby Dick"];
+
+function Book(myLibrary) {
+    for (let i = 0; i = myLibrary.length; i++) {
+        console.log(myLibrary)
+    }
+}
+
+function addBookToLibrary() {
+
+}
+
+const button = document.getElementById("new-book");
+element.addEventListener("click", addBookToLibrary);
