f5a497f51d071c75791100063adc9e87680a7a41
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2024-05-15 23:21:21 -0400

    html skeleton

--- script.js
+++ script.js
@@ -1,53 +1,21 @@
-// let myLibrary = ["Romeo and Juliet", "Hamlet", "Pride and Prejudice", 
-// "Moby Dick"];
+let myLibrary = [];
 
-// function Book(myLibrary) {
-//     for (let i = 0; i = myLibrary.length; i++) {
-//         console.log(myLibrary)
-//     }
-// }
+const newBook = document.getElementById("new-book")
 
+// Book constructor
+function Book (title, author, pages, read){
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+    this.info = function() {
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, `)
+    }
+}
+
+// Add book to Library
 // function addBookToLibrary() {
 
 // }
 
-// const button = document.getElementById("new-book");
-// element.addEventListener("click", addBookToLibrary);
-
-
-// Function that takes in an animal and prints "____ is my favorite animal"
-function favoriteAnimal(animal) {
-    return animal + " is my favorite animal!";
-}
-
-console.log(favoriteAnimal('el patito'));
-
-// Write a function called add7 that takes one number and returns that number + 7.
-function add7(number) {
-    return number + 7;
-}
-
-console.log(add7(7));
-
-// Write a function called multiply that takes 2 numbers and returns their product.
-function multiply(a, b) {
-    return a * b;
-}
-
-console.log(multiply(7, 2))
-
-// Write a function called capitalize that takes a string and returns that string with only the first letter capitalized.
-function capitalize(string) {
-    return string.uppercase
-}
-
-console.log(capitalize("MAM"))
-
-// Write a function called lastLetter that takes a string and returns the very last letter of that string:
-function lastLetter(str) {
-    return str.charAt(str.length - 1);
-}
-
-console.log(lastLetter())
-
-const newBook = document.getElementById("new-book")
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 879, false);
