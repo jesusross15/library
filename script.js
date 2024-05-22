dcc1727cac55f609dd07d6d5f0b8efe8692773e0
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2024-05-21 23:34:20 -0400

    adding event listeners

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const newBook = document.getElementById("new-book")
 
 // Book constructor
 function Book (title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
     this.info = function() {
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, `)
+        return (`${this.title} by ${this.author}, ${this.pages} pages, `)
     }
 }
 
@@ -18,4 +18,14 @@ function Book (title, author, pages, read){
 
 // }
 
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 879, false);
+
+// button event listeners to create a new book, add a new book to page, and to close popup
+  const addBtn = document.querySelector('#add-button');
+  addBtn.addEventListener('click', addBookToLibrary);
+  
+  const newBookBtn = document.querySelector('#new-book-button');
+  newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+  
+  const popUpForm = document.getElementById('new-book-card');
+  const closePopUp = document.getElementsByTagName('span')[0];
+  closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
