f68e7593c96f26d746809c1271db9ea1253778b3
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2024-05-24 23:30:18 -0400

    Working button

--- script.js
+++ script.js
@@ -8,24 +8,36 @@ function Book (title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return (`${this.title} by ${this.author}, ${this.pages} pages, `)
-    }
+    // this.info = function() {
+    //     return (`${this.title} by ${this.author}, ${this.pages} pages, `)
+    // }
 }
 
 // Add book to Library
-// function addBookToLibrary() {
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").checked;
 
-// }
+} 
 
 
 // button event listeners to create a new book, add a new book to page, and to close popup
-  const addBtn = document.querySelector('#add-button');
-  addBtn.addEventListener('click', addBookToLibrary);
+
+const newBookBtn = document.getElementById("new-book-button");
+
+newBookBtn.addEventListener("click", function() {
+    const newBookForm = document.getElementsByClassName("new-book-card")[0];
+    newBookForm.style.display = "block";
+})
   
-  const newBookBtn = document.querySelector('#new-book-button');
-  newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+const addBtn = document.querySelector("#add-button");
+addBtn.addEventListener("click", addBookToLibrary);
+
+//   const popUpForm = document.getElementById('new-book-card');
+//   const closePopUp = document.getElementsByTagName('span')[0];
+//   closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+ 
+
   
-  const popUpForm = document.getElementById('new-book-card');
-  const closePopUp = document.getElementsByTagName('span')[0];
-  closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
