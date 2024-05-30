79a091760dc02168d13dd5032b3728dfa2b2e4d2
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2024-05-29 23:47:51 -0400

    book card display and working add button

--- script.js
+++ script.js
@@ -13,13 +13,26 @@ function Book (title, author, pages, read){
     // }
 }
 
+function render() {
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+   
+    for (let i=0; i<myLibrary.length; i++){
+        createBook(myLibrary[i]);
+    }
+}
+
 // Add book to Library
 function addBookToLibrary() {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
     let read = document.querySelector("#read").checked;
-
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    render();
 } 
 
 
@@ -31,13 +44,26 @@ newBookBtn.addEventListener("click", function() {
     const newBookForm = document.getElementsByClassName("new-book-card")[0];
     newBookForm.style.display = "block";
 })
-  
+
 const addBtn = document.querySelector("#add-button");
 addBtn.addEventListener("click", addBookToLibrary);
 
-//   const popUpForm = document.getElementById('new-book-card');
-//   const closePopUp = document.getElementsByTagName('span')[0];
-//   closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
- 
+document.querySelector("#new-book-form").addEventListener("submit", function() {
+    event.preventDefault();
+    addBookToLibrary();
+})
+
+//Creates book visual in browser
+function render() {
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+   
+    for (let i=0; i<myLibrary.length; i++){
+        createBook(myLibrary[i]);
+    }
+}
 
   
+// const closePopUp = document.getElementsByTagName("span");
+// closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
